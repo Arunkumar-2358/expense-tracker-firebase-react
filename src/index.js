cd2eafab6fc7aba61646ef06d898cf280ac582d1
Arunkumar-2358 <aremella@gitam.in>
2025-09-15 21:50:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+// Set basename to your GitHub repo name
+const basename =
+  process.env.NODE_ENV === "production" ? "/expense-tracker-firebase-react" : "/";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={basename}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
