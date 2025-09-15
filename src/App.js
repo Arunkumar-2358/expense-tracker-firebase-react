1ca8f0b4397edda9538503f713af8bca99aa0127
Arunkumar-2358 <aremella@gitam.in>
2025-09-15 21:50:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Auth } from "./pages/auth/index";
 import { ExpenseTracker } from "./pages/expense-tracker/index";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Auth />} />
-          <Route path="/expense-tracker" element={<ExpenseTracker />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/expense-tracker" element={<ExpenseTracker />} />
+      </Routes>
     </div>
   );
 }
